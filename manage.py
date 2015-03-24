24558040bf6f911699e6a23942f2c4ae9feab262
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2015-03-24 20:51:30 +0100

    Remove migrate
    It's not used (yet) and causes problems with one of its dependencies.

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
 import os
 from flask.ext.script import Manager
-from flask.ext.migrate import MigrateCommand
 from tofuweb import app, db
 from tofuweb.models import Raw, Reconstruction
 
 
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 
 @manager.command
--- tofuweb/__init__.py
+++ tofuweb/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.migrate import Migrate
 from flask.ext.admin import Admin
 from flask.ext.restful import Api
 
@@ -13,7 +12,6 @@ app.config['SECRET_KEY'] = 'changesometimesoon'
 
 api = Api(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 admin = Admin(app)
 
 
